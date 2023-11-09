50c909db3c7fb3c4bf0f6e1571d67a436a410fee
marcello chiuminatto <mchiuminatto@gmail.com>
2023-11-08 21:22:49 -0300

    Added setup
    Signed-off-by: marcello chiuminatto <mchiuminatto@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='syntetick',
+    version='0.01',
+    packages=['synthetick'],
+    url='',
+    license='',
+    author='mchiuminatto',
+    author_email='mchiuminatto@gmail.com',
+    description='Library to produce synthetic price time series'
+)
